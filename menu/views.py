20e394fd101b0e6d924f44c8487ebc6371018b05
Pylos7 <nestor1ingles@gmail.com>
2024-05-07 08:50:29 -0400

    create view to see Items

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+from .models import Item
 
 # Create your views here.
+
+def item_list(request):
+    return HttpResponse("Hello World")
